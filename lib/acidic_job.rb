dbcff78029d7505d225ac3a107101db3a28ceaca
fractaledmind <stephen.margheim@gmail.com>
2021-06-20 06:48:19 +0200

    initial commit

---
+++ lib/acidic_job.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "acidic_job/version"
+
+module AcidicJob
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/acidic_job_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe AcidicJob do
+  it "has a version number" do
+    expect(AcidicJob::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
