8ed4e157e86e340b506b4efb1499fc80eefe72c1
fractaledmind <stephen.margheim@gmail.com>
2021-07-19 17:41:07 +0200

    Update code comments

--- lib/acidic_job.rb
+++ lib/acidic_job.rb
@@ -32,31 +32,32 @@ module AcidicJob
   end
 
   # Number of seconds passed which we consider a held idempotency key lock to be
-  # defunct and eligible to be locked again by a different API call. We try to
+  # defunct and eligible to be locked again by a different job run. We try to
   # unlock keys on our various failure conditions, but software is buggy, and
   # this might not happen 100% of the time, so this is a hedge against it.
   IDEMPOTENCY_KEY_LOCK_TIMEOUT = 90
 
-  # To try and enforce some level of required randomness in an idempotency key,
-  # we require a minimum length. This of course is a poor approximate, and in
-  # real life you might want to consider trying to measure actual entropy with
-  # something like the Shannon entropy equation.
-  IDEMPOTENCY_KEY_MIN_LENGTH = 20
-
   # &block
-  def idempotently(with:)
+  def idempotently(with:) # &block
     # set accessors for each argument passed in to ensure they are available
     # to the step methods the job will have written
     define_accessors_for_passed_arguments(with)
 
     # execute the block to gather the info on what phases are defined for this job
     defined_steps = yield
+    # [:create_ride_and_audit_record, :create_stripe_charge, :send_receipt]
 
     # convert the array of steps into a hash of recovery_points and callable actions
     phases = define_atomic_phases(defined_steps)
+    # { create_ride_and_audit_record: <#Method >, ... }
 
-    # find or create an AcidicJobKey record to store all information about this job
+    # find or create an AcidicJobKey record (our idempotency key) to store all information about this job
     # side-effect: will set the @key instance variable
+    #
+    # A key concept here is that if two requests try to insert or update within
+    # close proximity, one of the two will be aborted by Postgres because we're
+    # using a transaction with SERIALIZABLE isolation level. It may not look
+    # it, but this code is safe from races.
     ensure_idempotency_key_record(job_id, with, defined_steps.first)
 
     # if the key record is already marked as finished, immediately return its result
@@ -136,8 +137,7 @@ module AcidicJob
         # because the original job was long enough ago.
         raise LockedIdempotencyKey if @key.locked_at && @key.locked_at > Time.current - IDEMPOTENCY_KEY_LOCK_TIMEOUT
 
-        # Lock the key and update latest run unless the job is already
-        # finished.
+        # Lock the key and update latest run unless the job is already finished.
         @key.update!(last_run_at: Time.current, locked_at: Time.current) unless @key.finished?
       else
         @key = AcidicJobKey.create!(
