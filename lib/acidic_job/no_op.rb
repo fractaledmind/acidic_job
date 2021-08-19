0adf0dd3bd953adca888070ebd11195cc979fc58
fractaledmind <stephen.margheim@gmail.com>
2021-08-19 09:50:48 +0200

    Move return objects under AcidicJob namespace

--- lib/acidic_job/no_op.rb
+++ lib/acidic_job/no_op.rb
@@ -2,8 +2,10 @@
 
 # Represents an action to perform a no-op. One possible option for a return
 # from an #atomic_phase block.
-class NoOp
-  def call(_key)
-    # no-op
+module AcidicJob
+  class NoOp
+    def call(_key)
+      # no-op
+    end
   end
 end
--- lib/acidic_job/recovery_point.rb
+++ lib/acidic_job/recovery_point.rb
@@ -2,14 +2,16 @@
 
 # Represents an action to set a new recovery point. One possible option for a
 # return from an #atomic_phase block.
-class RecoveryPoint
-  attr_accessor :name
+module AcidicJob
+  class RecoveryPoint
+    attr_accessor :name
 
-  def initialize(name)
-    self.name = name
-  end
+    def initialize(name)
+      self.name = name
+    end
 
-  def call(key:)
-    key.update_column(:recovery_point, name)
+    def call(key:)
+      key.update_column(:recovery_point, name)
+    end
   end
 end
