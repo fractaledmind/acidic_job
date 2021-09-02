2513fd03068950965a89c43d97b8260c26c06301
fractaledmind <stephen.margheim@gmail.com>
2021-09-02 16:49:12 +0200

    Improve tests for staged jobs

--- test/acidic_job_test.rb
+++ test/acidic_job_test.rb
@@ -16,6 +16,7 @@ class TestAcidicJobs < Minitest::Test
     }.freeze
     @valid_user = User.find_by(stripe_customer_id: "tok_visa")
     @invalid_user = User.find_by(stripe_customer_id: "tok_chargeCustomerFail")
+    @staged_job_params = {amount: 20_00, currency: "usd", user: @valid_user}
     RideCreateJob.undef_method(:raise_error) if RideCreateJob.respond_to?(:raise_error)
   end
 
@@ -51,16 +52,16 @@ class TestAcidicJobs < Minitest::Test
 
   class IdempotencyKeysAndRecoveryTest < TestAcidicJobs
     def test_passes_for_a_new_key
-      assert_enqueued_with(job: SendRideReceiptJob, args: [{amount: 20_00,currency: "usd",user: @valid_user}]) do
+      assert_enqueued_with(job: SendRideReceiptJob, args: [@staged_job_params]) do
         result = RideCreateJob.perform_now(@valid_user, @valid_params)
-
         assert_equal true, result
-        assert_equal true, AcidicJob::Key.first.succeeded?
-        assert_equal 1, AcidicJob::Key.count
-        assert_equal 1, Ride.count
-        assert_equal 1, Audit.count
-        assert_equal 1, AcidicJob::Staging.count
       end
+
+      assert_equal true, AcidicJob::Key.first.succeeded?
+      assert_equal 1, AcidicJob::Key.count
+      assert_equal 1, Ride.count
+      assert_equal 1, Audit.count
+      assert_equal 1, AcidicJob::Staging.count
     end
 
     def test_returns_a_stored_result
@@ -81,8 +82,10 @@ class TestAcidicJobs < Minitest::Test
     def test_passes_for_keys_that_are_unlocked
       key = create_key(locked_at: nil)
       AcidicJob::Key.stub(:find_by, ->(*) { key }) do
-        result = RideCreateJob.perform_now(@valid_user, @valid_params)
-        assert_equal true, result
+        assert_enqueued_with(job: SendRideReceiptJob, args: [@staged_job_params]) do
+          result = RideCreateJob.perform_now(@valid_user, @valid_params)
+          assert_equal true, result
+        end
       end
       key.reload
 
@@ -96,8 +99,10 @@ class TestAcidicJobs < Minitest::Test
     def test_passes_for_keys_with_a_stale_locked_at
       key = create_key(locked_at: Time.now - 1.hour - 1)
       AcidicJob::Key.stub(:find_by, ->(*) { key }) do
-        result = RideCreateJob.perform_now(@valid_user, @valid_params)
-        assert_equal true, result
+        assert_enqueued_with(job: SendRideReceiptJob, args: [@staged_job_params]) do
+          result = RideCreateJob.perform_now(@valid_user, @valid_params)
+          assert_equal true, result
+        end
       end
       key.reload
 
@@ -130,8 +135,10 @@ class TestAcidicJobs < Minitest::Test
     def test_continues_from_recovery_point_create_ride_and_audit_record
       key = create_key(recovery_point: :create_ride_and_audit_record)
       AcidicJob::Key.stub(:find_by, ->(*) { key }) do
-        result = RideCreateJob.perform_now(@valid_user, @valid_params)
-        assert_equal true, result
+        assert_enqueued_with(job: SendRideReceiptJob, args: [@staged_job_params]) do
+          result = RideCreateJob.perform_now(@valid_user, @valid_params)
+          assert_equal true, result
+        end
       end
       key.reload
 
@@ -148,8 +155,10 @@ class TestAcidicJobs < Minitest::Test
         user: @valid_user
       ))
       AcidicJob::Key.stub(:find_by, ->(*) { key }) do
-        result = RideCreateJob.perform_now(@valid_user, @valid_params)
-        assert_equal true, result
+        assert_enqueued_with(job: SendRideReceiptJob, args: [@staged_job_params]) do
+          result = RideCreateJob.perform_now(@valid_user, @valid_params)
+          assert_equal true, result
+        end
       end
       key.reload
 
@@ -163,8 +172,10 @@ class TestAcidicJobs < Minitest::Test
     def test_continues_from_recovery_point_send_receipt
       key = create_key(recovery_point: :send_receipt)
       AcidicJob::Key.stub(:find_by, ->(*) { key }) do
-        result = RideCreateJob.perform_now(@valid_user, @valid_params)
-        assert_equal true, result
+        assert_enqueued_with(job: SendRideReceiptJob, args: [@staged_job_params]) do
+          result = RideCreateJob.perform_now(@valid_user, @valid_params)
+          assert_equal true, result
+        end
       end
       key.reload
 
