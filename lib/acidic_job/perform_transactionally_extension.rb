ead7ed1a4b1b94a5d24b9198f9085a5343e06275
fractaledmind <stephen.margheim@gmail.com>
2021-10-19 15:44:29 +0200

    Handle one or both libraries not being present

--- lib/acidic_job/perform_transactionally_extension.rb
+++ lib/acidic_job/perform_transactionally_extension.rb
@@ -9,13 +9,13 @@ module AcidicJob
     class_methods do
       # rubocop:disable Metrics/MethodLength
       def perform_transactionally(*args)
-        attributes = if self < ActiveJob::Base
+        attributes = if defined?(ActiveJob) && self < ActiveJob::Base
                        {
                          adapter: "activejob",
                          job_name: name,
                          job_args: job_or_instantiate(*args).serialize
                        }
-                     elsif include? Sidekiq::Worker
+                     elsif defined?(Sidekiq) && include?(Sidekiq::Worker)
                        {
                          adapter: "sidekiq",
                          job_name: name,
