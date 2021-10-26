14ce6465700211bdd7571a86f8f555e1f994fcf5
fractaledmind <stephen.margheim@gmail.com>
2021-10-26 21:47:58 +0200

    Add a global_id of the AcidicJob::Staged record to the arguments passed to the job
    which will be extracted by our custom `perform` wrappers

--- lib/acidic_job/staged.rb
+++ lib/acidic_job/staged.rb
@@ -1,10 +1,13 @@
 # frozen_string_literal: true
 
 require "active_record"
+require "global_id"
 
 module AcidicJob
   class Staged < ActiveRecord::Base
     self.table_name = "staged_acidic_jobs"
+
+    include GlobalID::Identification
 
     validates :adapter, presence: true
     validates :job_name, presence: true
@@ -15,18 +18,29 @@ module AcidicJob
     after_create_commit :enqueue_job
 
     def enqueue_job
+      gid = {"staged_job_gid" => self.to_global_id.to_s}
+
+      if job_args.is_a?(Hash) && job_args.key?("arguments")
+        job_args["arguments"].concat([gid])
+      else
+        job_args.concat([gid])
+      end
+
       case adapter
       when "activejob"
         job = ActiveJob::Base.deserialize(job_args)
         job.enqueue
       when "sidekiq"
-        Sidekiq::Client.push("class" => job_name, "args" => job_args)
+        Sidekiq::Client.push(
+          "class" => job_name,
+          "args" => job_args,
+        )
       else
         raise UnknownJobAdapter.new(adapter: adapter)
       end
 
-      # TODO: ensure successful enqueuing before deletion
-      delete
+      # NOTE: record will be deleted after the job has successfully been performed
+      true
     end
   end
 end
