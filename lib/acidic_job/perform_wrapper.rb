6979a9aa9aa3656eed4e0950022a205bd7421f34
fractaledmind <stephen.margheim@gmail.com>
2021-10-26 21:48:31 +0200

    Extract the passed `staged_job_gid` value, if passed
    from the wrapped `perform` method

--- lib/acidic_job/perform_wrapper.rb
+++ lib/acidic_job/perform_wrapper.rb
@@ -3,6 +3,14 @@
 module AcidicJob
   module PerformWrapper
     def perform(*args, **kwargs)
+      # extract the `staged_job_gid` if present
+      # so that we can later delete the record in an `after_perform` callback
+      final_arg = args.last
+      if final_arg.is_a?(Hash) && final_arg.key?("staged_job_gid")
+        args = args[0..-2]
+        @staged_job_gid = final_arg["staged_job_gid"]
+      end
+
       # store arguments passed into `perform` so that we can later persist
       # them to `AcidicJob::Key#job_args` for both ActiveJob and Sidekiq::Worker
       @arguments_for_perform = if args.any? && kwargs.any?
@@ -15,7 +23,7 @@ module AcidicJob
                                  []
                                end
 
-      super
+      super(*args, **kwargs)
     end
   end
 end
