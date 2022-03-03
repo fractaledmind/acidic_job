d1fc054d164cd22e29729d3617313a48c2c4fe4a
Stephen Margheim <stephen.margheim@gmail.com>
2022-03-03 14:15:15 +0100

    Improve Noticed extension test

--- test/acidic_job/extensions/noticed_test.rb
+++ test/acidic_job/extensions/noticed_test.rb
@@ -19,6 +19,16 @@ class TestNoticedExtension < Minitest::Test
   def setup
     @user = User.find_by(stripe_customer_id: "tok_visa")
   end
+  
+  def before_setup
+    super
+    DatabaseCleaner.start
+  end
+  
+  def after_teardown
+    DatabaseCleaner.clean
+    super
+  end
 
   def test_deliver_acidicly_on_noticed_notification_with_only_database_delivery
     OnlyDatabaseNotification.deliver_acidicly(@user)
@@ -30,5 +40,9 @@ class TestNoticedExtension < Minitest::Test
     ExampleNotification.deliver_acidicly(@user)
 
     assert_equal 1, AcidicJob::Run.staged.count
+    
+    noticed_run = AcidicJob::Run.staged.first
+    assert_equal "Noticed::DeliveryMethods::Test", noticed_run.job_class
+    assert_equal "default", noticed_run.serialized_job["queue_name"]
   end
 end
