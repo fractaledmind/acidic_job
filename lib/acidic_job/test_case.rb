fac20707f18e9d68093711a36993d14daa5c8219
Stephen Margheim <stephen.margheim@gmail.com>
2022-03-11 13:57:15 +0100

    Add some testing help to the README and with support files

---
+++ lib/acidic_job/test_case.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require "active_job/test_case"
+require "database_cleaner/active_record"
+
+module AcidicJob
+  class TestCase < ActiveJob::TestCase
+    self.use_transactional_tests = false
+
+    def before_setup
+      super
+      DatabaseCleaner.start
+    end
+
+    def after_teardown
+      DatabaseCleaner.clean
+      super
+    end
+  end
+end
