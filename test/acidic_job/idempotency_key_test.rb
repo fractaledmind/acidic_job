82e4fb3f03376c8520094321d80eee3224fc3c75
Stephen Margheim <stephen.margheim@gmail.com>
2022-03-16 18:15:40 +0100

    Add tests for IdempotencyKey service and Procs

--- test/acidic_job/idempotency_key_test.rb
+++ test/acidic_job/idempotency_key_test.rb
@@ -3,6 +3,7 @@
 require "test_helper"
 
 class TestIdempotencyKey < Minitest::Test
+  # with job_id acidic identifier
   def test_return_job_id_from_hash_when_identifier_job_id
     value = AcidicJob::IdempotencyKey.new(:job_id).value_for({ "job_id" => "ID" })
 
@@ -49,6 +50,8 @@ class TestIdempotencyKey < Minitest::Test
     assert_equal "2370d4813b8b5985f2363681034e3fc312988344", value
   end
 
+  # with job_args acidic identifier
+
   def test_return_job_id_from_hash_when_identifier_job_args
     value = AcidicJob::IdempotencyKey.new(:job_args).value_for({ "job_id" => "ID" })
 
@@ -93,5 +96,19 @@ class TestIdempotencyKey < Minitest::Test
     value = AcidicJob::IdempotencyKey.new(:job_args).value_for(job.new(klass.new("SomeClass")))
 
     assert_equal "2370d4813b8b5985f2363681034e3fc312988344", value
+  end
+
+  # with proc acidic identifier
+
+  def test_return_identifier_proc_value_from_args
+    value = AcidicJob::IdempotencyKey.new(->(int) { int**2 }).value_for({ "job_id" => "ID" }, 2)
+
+    assert_equal "1b6453892473a467d07372d45eb05abc2031647a", value
+  end
+
+  def test_return_identifier_proc_value_from_kwargs
+    value = AcidicJob::IdempotencyKey.new(->(int:) { int**2 }).value_for({ "job_id" => "ID" }, int: 2)
+
+    assert_equal "1b6453892473a467d07372d45eb05abc2031647a", value
   end
 end
