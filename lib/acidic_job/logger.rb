523b638f54c1be235f7089fa3944e09bcad612eb
Stephen Margheim <stephen.margheim@gmail.com>
2022-05-23 23:49:23 +0200

    Use a tagged logger with a `log_run_event` helper method

--- lib/acidic_job/logger.rb
+++ lib/acidic_job/logger.rb
@@ -1,15 +1,31 @@
 # frozen_string_literal: true
 
 require "logger"
+require "active_support/tagged_logging"
 
 module AcidicJob
-  class Logger < ::Logger; end
+  class Logger < ::Logger
+    def log_run_event(msg, job = nil, run = nil)
+      tags = [
+        run&.idempotency_key,
+        inspect_name(job)
+      ].compact
+
+      tagged(*tags) { debug(msg) }
+    end
+
+    def inspect_name(obj)
+      return if obj.nil?
+
+      obj.inspect.split.first.remove("#<")
+    end
+  end
 
   def self.logger
-    @logger ||= AcidicJob::Logger.new($stdout, level: :info)
+    @logger ||= ActiveSupport::TaggedLogging.new(AcidicJob::Logger.new($stdout, level: :debug))
   end
 
   def self.logger=(new_logger)
-    @logger = new_logger
+    @logger = ActiveSupport::TaggedLogging.new(new_logger)
   end
 end
