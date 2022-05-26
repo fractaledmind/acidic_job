f3e8a43a30810151b858652906847fdbdabb60b8
Stephen Margheim <stephen.margheim@gmail.com>
2022-05-26 12:52:19 +0200

    Update the workflow test

--- test/workflow_test.rb
+++ test/workflow_test.rb
@@ -1,10 +1,15 @@
 # frozen_string_literal: true
 
 require "test_helper"
-require_relative "support/sidekiq_batches"
+require_relative "support/sidekiq_testing"
 require_relative "support/test_case"
 
 class CustomErrorForTesting < StandardError; end
+
+class ApplicationWorker
+  include Sidekiq::Worker
+  include AcidicJob
+end
 
 class TestWorkflows < TestCase
   def setup
@@ -12,11 +17,8 @@ class TestWorkflows < TestCase
   end
 
   def test_step_with_awaits_is_run_properly
-    dynamic_class = Class.new(Support::Sidekiq::Workflow) do
-      include Sidekiq::Worker
-      include AcidicJob
-
-      dynamic_step_job = Class.new(Support::Sidekiq::StepWorker) do
+    dynamic_class = Class.new(ApplicationWorker) do
+      dynamic_step_job = Class.new(ApplicationWorker) do
         def perform; end
       end
       Object.const_set("SuccessfulAsyncWorker", dynamic_step_job)
@@ -32,17 +34,20 @@ class TestWorkflows < TestCase
     WorkerWithSuccessfulAwaitStep.new.perform
     Sidekiq::Worker.drain_all
 
-    assert_equal 1, AcidicJob::Run.count
-    assert_equal "FINISHED", AcidicJob::Run.first.recovery_point
+    assert_equal 2, AcidicJob::Run.count
+
+    parent_run = AcidicJob::Run.find_by(job_class: "WorkerWithSuccessfulAwaitStep")
+    assert_equal "FINISHED", parent_run.recovery_point
+
+    child_run = AcidicJob::Run.find_by(job_class: "SuccessfulAsyncWorker")
+    assert_nil child_run.recovery_point
+
     assert_equal 0, Sidekiq::RetrySet.new.size
   end
 
   def test_step_with_awaits_job_that_errors_does_not_progress_run_and_does_not_store_error_object_but_does_retry
-    dynamic_class = Class.new(Support::Sidekiq::Workflow) do
-      include Sidekiq::Worker
-      include AcidicJob
-
-      dynamic_step_job = Class.new(Support::Sidekiq::StepWorker) do
+    dynamic_class = Class.new(ApplicationWorker) do
+      dynamic_step_job = Class.new(ApplicationWorker) do
         def perform
           raise CustomErrorForTesting
         end
@@ -63,25 +68,30 @@ class TestWorkflows < TestCase
       Sidekiq::Worker.drain_all
     end
 
-    assert_equal 1, AcidicJob::Run.count
-    assert_equal "await_step", AcidicJob::Run.first.recovery_point
-    assert_nil AcidicJob::Run.first.error_object
-    assert_equal 1, Sidekiq::RetrySet.new.size
+    assert_equal 2, AcidicJob::Run.count
+
+    parent_run = AcidicJob::Run.find_by(job_class: "WorkerWithErroringAwaitStep")
+    assert_equal "await_step", parent_run.recovery_point
+
+    child_run = AcidicJob::Run.find_by(job_class: "ErroringAsyncWorker")
+    assert_nil child_run.recovery_point
+    assert_nil child_run.error_object
+
+    retry_set = Sidekiq::RetrySet.new
+    assert_equal 1, retry_set.size
+    assert_equal ["ErroringAsyncWorker"], retry_set.map { _1.item["class"] }
   end
 
   def test_step_with_nested_awaits_jobs_is_run_properly
-    dynamic_class = Class.new(Support::Sidekiq::Workflow) do
-      include Sidekiq::Worker
-      include AcidicJob
-
-      erroring_async_worker = Class.new(Support::Sidekiq::StepWorker) do
+    dynamic_class = Class.new(ApplicationWorker) do
+      successful_async_worker = Class.new(ApplicationWorker) do
         def perform
           true
         end
       end
-      Object.const_set("NestedSuccessfulWorker", erroring_async_worker)
+      Object.const_set("NestedSuccessfulWorker", successful_async_worker)
 
-      nested_awaits_job = Class.new(Support::Sidekiq::StepWorker) do
+      nested_awaits_job = Class.new(ApplicationWorker) do
         def perform
           with_acidity providing: {} do
             step :await_nested_step, awaits: [NestedSuccessfulWorker]
@@ -99,28 +109,32 @@ class TestWorkflows < TestCase
     Object.const_set("WorkerWithSuccessfulNestedAwaitSteps", dynamic_class)
 
     WorkerWithSuccessfulNestedAwaitSteps.new.perform
-
     Sidekiq::Worker.drain_all
 
-    assert_equal 2, AcidicJob::Run.count
-    assert_equal "FINISHED", AcidicJob::Run.first.recovery_point
-    assert_equal "FINISHED", AcidicJob::Run.second.recovery_point
+    assert_equal 3, AcidicJob::Run.count
+
+    parent_run = AcidicJob::Run.find_by(job_class: "WorkerWithSuccessfulNestedAwaitSteps")
+    assert_equal "FINISHED", parent_run.recovery_point
+
+    child_run = AcidicJob::Run.find_by(job_class: "NestedSuccessfulAwaitsWorker")
+    assert_equal "FINISHED", child_run.recovery_point
+
+    grandchild_run = AcidicJob::Run.find_by(job_class: "NestedSuccessfulWorker")
+    assert_nil grandchild_run.recovery_point
+
     assert_equal 0, Sidekiq::RetrySet.new.size
   end
 
   def test_step_with_nested_awaits_jobs_that_errors_in_second_level
-    dynamic_class = Class.new(Support::Sidekiq::Workflow) do
-      include Sidekiq::Worker
-      include AcidicJob
-
-      erroring_async_worker = Class.new(Support::Sidekiq::StepWorker) do
+    dynamic_class = Class.new(ApplicationWorker) do
+      erroring_async_worker = Class.new(ApplicationWorker) do
         def perform
           raise CustomErrorForTesting
         end
       end
       Object.const_set("NestedErroringWorker", erroring_async_worker)
 
-      nested_awaits_job = Class.new(Support::Sidekiq::StepWorker) do
+      nested_awaits_job = Class.new(ApplicationWorker) do
         def perform
           with_acidity providing: {} do
             step :await_nested_step, awaits: [NestedErroringWorker]
@@ -143,9 +157,16 @@ class TestWorkflows < TestCase
       Sidekiq::Worker.drain_all
     end
 
-    assert_equal 2, AcidicJob::Run.count
-    assert_equal "await_step", AcidicJob::Run.first.recovery_point
-    assert_equal "await_nested_step", AcidicJob::Run.second.recovery_point
+    assert_equal 3, AcidicJob::Run.count
+
+    parent_run = AcidicJob::Run.find_by(job_class: "WorkerWithErroringNestedAwaitSteps")
+    assert_equal "await_step", parent_run.recovery_point
+
+    child_run = AcidicJob::Run.find_by(job_class: "NestedErroringAwaitsWorker")
+    assert_equal "await_nested_step", child_run.recovery_point
+
+    grandchild_run = AcidicJob::Run.find_by(job_class: "NestedErroringWorker")
+    assert_nil grandchild_run.recovery_point
 
     retry_set = Sidekiq::RetrySet.new
     assert_equal 1, retry_set.size
@@ -153,11 +174,8 @@ class TestWorkflows < TestCase
   end
 
   def test_step_with_awaits_that_takes_args_is_run_properly
-    dynamic_class = Class.new(Support::Sidekiq::Workflow) do
-      include Sidekiq::Worker
-      include AcidicJob
-
-      dynamic_step_job = Class.new(Support::Sidekiq::StepWorker) do
+    dynamic_class = Class.new(ApplicationWorker) do
+      dynamic_step_job = Class.new(ApplicationWorker) do
         def perform(arg); end
       end
       Object.const_set("SuccessfulArgWorker", dynamic_step_job)
@@ -173,100 +191,158 @@ class TestWorkflows < TestCase
     WorkerWithSuccessfulArgAwaitStep.new.perform
     Sidekiq::Worker.drain_all
 
-    assert_equal 1, AcidicJob::Run.count
-    assert_equal "FINISHED", AcidicJob::Run.first.recovery_point
+    assert_equal 2, AcidicJob::Run.count
+
+    parent_run = AcidicJob::Run.find_by(job_class: "WorkerWithSuccessfulArgAwaitStep")
+    assert_equal "FINISHED", parent_run.recovery_point
+
+    child_run = AcidicJob::Run.find_by(job_class: "SuccessfulArgWorker")
+    assert_nil child_run.recovery_point
+
     assert_equal 0, Sidekiq::RetrySet.new.size
   end
 
   def test_step_with_dynamic_awaits_as_symbol_is_run_properly
-    dynamic_class = Class.new(Support::Sidekiq::Workflow) do
-      include Sidekiq::Worker
-      include AcidicJob
-
-      successful_step_job = Class.new(Support::Sidekiq::StepWorker) do
+    dynamic_class = Class.new(ApplicationWorker) do
+      successful_step_job = Class.new(ApplicationWorker) do
         def perform(arg); end
       end
       Object.const_set("SuccessfulDynamicAwaitFromSymbolWorker", successful_step_job)
 
-      erroring_step_job = Class.new(Support::Sidekiq::StepWorker) do
-        def perform
-          raise CustomErrorForTesting
-        end
-      end
-      Object.const_set("ErroringDynamicAwaitFromSymbolWorker", erroring_step_job)
-
-      def perform(bool)
-        @bool = bool
-
+      def perform
         with_acidity providing: {} do
           step :await_step, awaits: :dynamic_awaiting
         end
       end
 
       def dynamic_awaiting
-        return [SuccessfulDynamicAwaitFromSymbolWorker.with(123)] if @bool
-
-        [ErroringDynamicAwaitFromSymbolWorker]
+        [SuccessfulDynamicAwaitFromSymbolWorker.with(123)]
       end
     end
-    Object.const_set("WorkerWithDynamicAwaitsAsSymbol", dynamic_class)
+    Object.const_set("WorkerWithSuccessfulDynamicAwaitsAsSymbol", dynamic_class)
 
-    WorkerWithDynamicAwaitsAsSymbol.new.perform(true)
+    WorkerWithSuccessfulDynamicAwaitsAsSymbol.new.perform
     Sidekiq::Worker.drain_all
 
-    assert_equal 1, AcidicJob::Run.count
-    assert_equal "FINISHED", AcidicJob::Run.first.recovery_point
-    assert_equal 0, Sidekiq::RetrySet.new.size
+    assert_equal 2, AcidicJob::Run.count
 
-    WorkerWithDynamicAwaitsAsSymbol.new.perform(false)
-    assert_raises Sidekiq::JobRetry::Handled do
-      Sidekiq::Worker.drain_all
-    end
+    parent_run = AcidicJob::Run.find_by(job_class: "WorkerWithSuccessfulDynamicAwaitsAsSymbol")
+    assert_equal "FINISHED", parent_run.recovery_point
+
+    child_run = AcidicJob::Run.find_by(job_class: "SuccessfulDynamicAwaitFromSymbolWorker")
+    assert_nil child_run.recovery_point
+
+    assert_equal 0, Sidekiq::RetrySet.new.size
   end
 
-  def test_step_with_dynamic_awaits_as_string_is_run_properly
-    dynamic_class = Class.new(Support::Sidekiq::Workflow) do
-      include Sidekiq::Worker
-      include AcidicJob
-
-      successful_step_job = Class.new(Support::Sidekiq::StepWorker) do
-        def perform(arg); end
-      end
-      Object.const_set("SuccessfulDynamicAwaitFromStringWorker", successful_step_job)
-
-      erroring_step_job = Class.new(Support::Sidekiq::StepWorker) do
+  def test_step_with_dynamic_awaits_as_symbol_that_errors
+    dynamic_class = Class.new(ApplicationWorker) do
+      erroring_step_job = Class.new(ApplicationWorker) do
         def perform
           raise CustomErrorForTesting
         end
       end
       Object.const_set("ErroringDynamicAwaitFromSymbolWorker", erroring_step_job)
 
-      def perform(bool)
-        @bool = bool
+      def perform
+        with_acidity providing: {} do
+          step :await_step, awaits: :dynamic_awaiting
+        end
+      end
 
+      def dynamic_awaiting
+        [ErroringDynamicAwaitFromSymbolWorker]
+      end
+    end
+    Object.const_set("WorkerWithErroringDynamicAwaitsAsSymbol", dynamic_class)
+
+    WorkerWithErroringDynamicAwaitsAsSymbol.new.perform
+    assert_raises Sidekiq::JobRetry::Handled do
+      Sidekiq::Worker.drain_all
+    end
+
+    assert_equal 2, AcidicJob::Run.count
+
+    parent_run = AcidicJob::Run.find_by(job_class: "WorkerWithErroringDynamicAwaitsAsSymbol")
+    assert_equal "await_step", parent_run.recovery_point
+
+    child_run = AcidicJob::Run.find_by(job_class: "ErroringDynamicAwaitFromSymbolWorker")
+    assert_nil child_run.recovery_point
+
+    retry_set = Sidekiq::RetrySet.new
+    assert_equal 1, retry_set.size
+    assert_equal ["ErroringDynamicAwaitFromSymbolWorker"], retry_set.map { _1.item["class"] }
+  end
+
+  def test_step_with_dynamic_awaits_as_string_is_run_properly
+    dynamic_class = Class.new(ApplicationWorker) do
+      successful_step_job = Class.new(ApplicationWorker) do
+        def perform(arg); end
+      end
+      Object.const_set("SuccessfulDynamicAwaitFromStringWorker", successful_step_job)
+
+      def perform
         with_acidity providing: {} do
           step :await_step, awaits: "dynamic_awaiting"
         end
       end
 
       def dynamic_awaiting
-        return [SuccessfulDynamicAwaitFromStringWorker.with(123)] if @bool
-
-        [ErroringDynamicAwaitFromSymbolWorker]
+        [SuccessfulDynamicAwaitFromStringWorker.with(123)]
       end
     end
-    Object.const_set("WorkerWithDynamicAwaitsAsString", dynamic_class)
+    Object.const_set("WorkerWithSuccessfulDynamicAwaitsAsString", dynamic_class)
 
-    WorkerWithDynamicAwaitsAsString.new.perform(true)
+    WorkerWithSuccessfulDynamicAwaitsAsString.new.perform
     Sidekiq::Worker.drain_all
 
-    assert_equal 1, AcidicJob::Run.count
-    assert_equal "FINISHED", AcidicJob::Run.first.recovery_point
-    assert_equal 0, Sidekiq::RetrySet.new.size
+    assert_equal 2, AcidicJob::Run.count
 
-    WorkerWithDynamicAwaitsAsString.new.perform(false)
+    parent_run = AcidicJob::Run.find_by(job_class: "WorkerWithSuccessfulDynamicAwaitsAsString")
+    assert_equal "FINISHED", parent_run.recovery_point
+
+    child_run = AcidicJob::Run.find_by(job_class: "SuccessfulDynamicAwaitFromStringWorker")
+    assert_nil child_run.recovery_point
+
+    assert_equal 0, Sidekiq::RetrySet.new.size
+  end
+
+  def test_step_with_dynamic_awaits_as_string_that_errors
+    dynamic_class = Class.new(ApplicationWorker) do
+      erroring_step_job = Class.new(ApplicationWorker) do
+        def perform
+          raise CustomErrorForTesting
+        end
+      end
+      Object.const_set("ErroringDynamicAwaitFromStringWorker", erroring_step_job)
+
+      def perform
+        with_acidity providing: {} do
+          step :await_step, awaits: "dynamic_awaiting"
+        end
+      end
+
+      def dynamic_awaiting
+        [ErroringDynamicAwaitFromStringWorker]
+      end
+    end
+    Object.const_set("WorkerWithErroringDynamicAwaitsAsString", dynamic_class)
+
+    WorkerWithErroringDynamicAwaitsAsString.new.perform
     assert_raises Sidekiq::JobRetry::Handled do
       Sidekiq::Worker.drain_all
     end
+
+    assert_equal 2, AcidicJob::Run.count
+
+    parent_run = AcidicJob::Run.find_by(job_class: "WorkerWithErroringDynamicAwaitsAsString")
+    assert_equal "await_step", parent_run.recovery_point
+
+    child_run = AcidicJob::Run.find_by(job_class: "ErroringDynamicAwaitFromStringWorker")
+    assert_nil child_run.recovery_point
+
+    retry_set = Sidekiq::RetrySet.new
+    assert_equal 1, retry_set.size
+    assert_equal ["ErroringDynamicAwaitFromStringWorker"], retry_set.map { _1.item["class"] }
   end
 end
