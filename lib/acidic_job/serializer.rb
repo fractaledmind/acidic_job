fcaf6bc937bb0000356766fe1d104b3a0ff73b7c
Stephen Margheim <stephen.margheim@gmail.com>
2022-08-03 11:58:58 +0200

    Fix the job serializer to work with Ruby 2.7

--- lib/acidic_job/serializer.rb
+++ lib/acidic_job/serializer.rb
@@ -31,8 +31,15 @@ class JobSerializer < ActiveJob::Serializers::ObjectSerializer
   def deserialize(hash)
     job = ActiveJob::Base.deserialize(hash)
     job.send(:deserialize_arguments_if_needed)
-    job.instance_variable_set(:@__acidic_job_args, job.arguments)
-    # job.instance_variable_set(:@__acidic_job_kwargs, kwargs)
+    if job.arguments.last.is_a?(Hash)
+      *args, kwargs = job.arguments
+    else
+      args = job.arguments
+      kwargs = {}
+    end
+    job.instance_variable_set(:@__acidic_job_args, args)
+    job.instance_variable_set(:@__acidic_job_kwargs, kwargs)
+
     job
   end
 
