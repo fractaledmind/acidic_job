207722d5848b224ec45ac05f2fb53d8c12c81ad0
Stephen Margheim <stephen.margheim@gmail.com>
2025-05-16 11:54:16 +0200

    Replace manual predicate methods with scopes and is_action? method on Entry

--- app/models/acidic_job/entry.rb
+++ app/models/acidic_job/entry.rb
@@ -4,16 +4,12 @@ module AcidicJob
   class Entry < Record
     belongs_to :execution, class_name: "AcidicJob::Execution"
 
-    def started?
-      action == "started"
-    end
+    scope :for_step, ->(step) { where(step: step) }
+    scope :for_action, ->(action) { where(action: action) }
+    scope :ordered, -> { order(timestamp: :asc) }
 
-    def succeeded?
-      action == "succeeded"
-    end
+    def self.most_recent = order(created_at: :desc).first
 
-    def errored?
-      action == "errored"
-    end
+    def is_action?(check) = self.action == check
   end
 end
