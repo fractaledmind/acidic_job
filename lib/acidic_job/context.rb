64ae1b16503896fe31f7e91b57d17b7356b0e97b
Stephen Margheim <stephen.margheim@gmail.com>
2025-06-12 17:35:25 +0200

    Have [] and []= simply delegate to set and get

--- lib/acidic_job/context.rb
+++ lib/acidic_job/context.rb
@@ -29,22 +29,11 @@ module AcidicJob
 
     # TODO: deprecate these methods
     def []=(key, value)
-      AcidicJob.instrument(:set_context, key: key, value: value) do
-        AcidicJob::Value.upsert(
-          {
-            execution_id: @execution.id,
-            key: key,
-            value: value,
-          },
-          unique_by: %i[execution_id key]
-        )
-      end
+      set(key => value)
     end
 
     def [](key)
-      AcidicJob.instrument(:get_context, key: key) do
-        @execution.values.select(:value).find_by(key: key)&.value
-      end
+      get(key).first
     end
   end
 end
