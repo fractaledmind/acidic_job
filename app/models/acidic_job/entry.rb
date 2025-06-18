452672117debc3e1175c814398b59d9c70b89933
Stephen Margheim <stephen.margheim@gmail.com>
2025-06-19 00:10:01 +0200

    Fix the ordered scope since MySQL has timestamps with matching values, so break values with created_at

--- app/models/acidic_job/entry.rb
+++ app/models/acidic_job/entry.rb
@@ -8,7 +8,7 @@ module AcidicJob
 
     scope :for_step, -> (step) { where(step: step) }
     scope :for_action, -> (action) { where(action: action) }
-    scope :ordered, -> { order(timestamp: :asc) }
+    scope :ordered, -> { order(timestamp: :asc, created_at: :asc) }
 
     def self.most_recent
       order(created_at: :desc).first
