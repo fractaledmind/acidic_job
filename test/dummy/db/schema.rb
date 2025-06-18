ee5cc6cfda1193f895589cba45c5cbffc4f667f0
Stephen Margheim <stephen.margheim@gmail.com>
2025-06-19 00:33:01 +0200

    Use the minimum Rails version as the schema version for tests

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_22_154837) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_22_154837) do
   create_table "acidic_job_entries", force: :cascade do |t|
     t.bigint "execution_id", null: false
     t.string "step", null: false
